cb6e2400962cf467785f7be40873493b39adb02f
tfilipow7 <43181506+tfilipow7@users.noreply.github.com>
2020-12-15 21:34:35 -0700

    Did Assignment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # loops_predict
+
+Howdy!
+
+Predict #1
+The code will execute and print 0 to 14. 
+
+Predict #2
+The console will print 3, 6, & 9.
+
+Predict #3
+Console Log will print:
+Line    Value
+1       1
+2       4
+3       5
+4       8
+5       10
+6       11
+7       14
+8       16
---
+++ index.js
@@ -0,0 +1,27 @@
+for (var name = 5; name <=10; name++) {
+    console.log(name);
+}
+
+
+// Predict 1
+for(var num = 0; num < 15; num++){
+    console.log(num);
+}
+
+// Predict 2
+for(var i = 1; i < 10; i+=2){
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}
+
+// Predict 3
+for(var j = 1; j <= 15; j++){
+    if(j % 2 == 0){
+        j+=2;
+    }
+    else if(j % 3 == 0){
+        j++;
+    }
+    console.log(j);
+}
